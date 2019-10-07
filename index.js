042ba040860e1791514e6e4f37f6b5678264efa8
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-10-07 13:40:38 -0700

    Starting CRUD, get and get by id working

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const server = require('./server.js');
+const server = require("./server.js");
 
-const PORT = process.env.PORT || 4000;
+const accountsRouter = require("./accounts/accounts-router");
+
+server.use("/accounts", accountsRouter);
+
+const PORT = process.env.PORT || 4100;
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
