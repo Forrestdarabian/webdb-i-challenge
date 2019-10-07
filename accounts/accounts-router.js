e42423e9429646bd52a1cc3fb6de9cfa469aa3e4
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-10-07 14:00:24 -0700

    Have gets, a post, a put, and a delete, now gotta get them working in insomnia

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -28,4 +28,40 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.post("/", (req, res) => {
+  const postData = req.body;
+  db("posts")
+    .insert(postData, "id")
+    .then(ids => {
+      res.status(200).json(ids);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.put("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  db("accounts")
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
 module.exports = router;
